06cf4b4f6448884b69eae403ab3dba77d3ea5383
bilal <bilal@flooid.in>
2024-12-20 20:29:02 +0530

    Updated

--- jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  testEnvironment: "node", // or "jsdom" depending on your project
-  maxWorkers: 2,  // Try reducing the number of workers
-};
--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // appDir: true,
-  },
-  webpack(config, { isServer }) {
-    // Ignore test files during the build
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.(test|spec)\.(js|jsx|ts|tsx)$/,
-        use: ['ignore-loader'],
-      });
-    }
+}
 
-    // Disable Jest workers
-    config.resolve.alias['jest-worker'] = false;
-
-    return config;
-  },
-};
-
-export default nextConfig;
+export default nextConfig
